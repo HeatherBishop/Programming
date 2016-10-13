6d48d0ff31adb9647113a78d6249927cf012b54a
HeatherBishop <s177801@ucs.ac.uk>
2016-10-13 20:06:10 +0100

    Add files via upload

---
+++ CoinCounter/CoinCounter/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinCounter
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> coins = new List<int>();
+            List<int> demoninations = new List<int>() { 1, 2, 5, 10, 20, 50 };
+
+            int coinage = 0;
+            bool valid = false;
+
+
+
+            while(!valid)
+            {            
+                //work out change for 27
+                Console.Write("How much change is required?");
+                Console.ReadLine();            
+
+                string numCoins = Console.ReadLine();
+                //convert string to int
+                valid = Int32.TryParse(numCoins, out coinage);
+            Change(coins, demoninations, 0, 0, coinage);
+            }
+
+
+        }
+        static void Change(List<int> coins, List<int> denominations, int highest, int sum, int goal)
+        {
+            //have we reached the goal(set by user)
+            if (sum == goal)
+            {
+                Display(coins, denominations);
+                return;
+            }
+
+            //is there too much?
+            if (sum > goal)
+            {
+                return;
+            }
+
+            //loop through denominations
+
+            foreach (int value in denominations)
+            {
+                //only add higher or equal denominations
+                if (value >= highest)
+                {
+                    List<int> copy = new List<int>(coins);
+                    copy.Add(value);
+                    Change(copy, denominations, value, sum + value, goal);
+                }
+            }
+        }
+
+        static void Display(List<int> coins, List<int> denominations)
+        {
+            foreach (int denomination in denominations)
+            {
+                int count = coins.Count(value => value == denomination);
+                Console.WriteLine("{0}: {1}", 
+                    denomination, 
+                    count);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        
+       
+    }
+    
+}
---
+++ CoinCounter/CoinCounter/Program2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinCounter
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //user input
+            double coinage = 0;
+           // decimal coins;
+            bool valid = true;
+
+            //coin denominations
+            double fifty = 0.50;
+            double twenty = 0.20;
+            double ten = 0.10;
+            double five = 0.05;
+            double two = 0.02;
+            double one = 0.01;
+
+            //number of required coins
+            int no50s = 0;
+            int no20s = 0;
+            int no10s = 0;
+            int no5s = 0;
+            int no2s = 0;
+            int no1s = 0;
+
+
+
+
+            while (valid)
+            {
+                Console.Write("How much change is required?");
+
+                string currentEntry = Console.ReadLine();
+                // convert string to double                
+              if(Double.TryParse(currentEntry, out coinage))
+                valid = false;
+
+            }
+
+        
+
+
+            while (coinage >= fifty)
+                {
+                    coinage -= fifty;
+                    no50s++;                
+            }
+                while (coinage >= twenty)
+                {
+                    coinage -= twenty;
+                    no20s++;
+                }
+                while (coinage >= ten)
+                {
+                    coinage -= ten;
+                    no10s++;
+                }
+
+
+                while (coinage >= five)
+                {
+                    coinage -= five;
+                    no5s++;
+                }
+
+
+                while (coinage >= two)
+                {
+                    coinage -= two;
+                    no2s++;
+                }
+
+                while (coinage >= one)
+                {
+                    coinage -= one;
+                    no1s++;
+
+                }
+                if (coinage <= 0)
+                {
+                    coinage = 0;
+                    valid = true;
+                }
+
+
+
+
+
+            Console.WriteLine("Number of 50ps: "+ no50s);
+            Console.WriteLine("Number of 20ps: "+ no20s);
+            Console.WriteLine("Number of 10ps: "+ no10s);
+            Console.WriteLine("Number of 5ps: "+ no5s);
+            Console.WriteLine("Number of 2ps: "+ no2s);
+            Console.WriteLine("Number of 1ps: "+ no1s);
+
+
+            
+            Console.ReadLine();
+
+        }
+
+
+        
+       
+    }
+    
+}
